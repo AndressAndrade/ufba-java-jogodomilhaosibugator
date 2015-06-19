56746fef09f40b9503c0f752c4a6950bb8835bca
fernandabarros <nanda_cbarros@hotmail.com>
2015-06-19 06:03:22 -0300

    fazendo o teste com uma pergunta

--- ShowdoMilhao/src/showmilhao/poo/ufba/JanelaPerguntas.java
+++ ShowdoMilhao/src/showmilhao/poo/ufba/JanelaPerguntas.java
@@ -6,14 +6,15 @@ import javax.swing.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
 
 public class JanelaPerguntas implements ActionListener {
 
 	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 	
-	JLabel	pergunta;
-	JButton res[] = new JButton[4];
+	static  JLabel	pergunta;
+	static  JButton res[] = new JButton[4];
 	JButton parar = new JButton();
 	JButton  pular;
 	
@@ -25,9 +26,86 @@ public class JanelaPerguntas implements ActionListener {
 	
 	int nDePulos = 3;
 	
+	
 	public static void main(String[] args) {
 		
+		//um molde de 9 perguntas aleatórias
+		
+		List<String> rq1 = new ArrayList<String>();
+		Questao q1 = new Questao("Qual era a cor do cavalo branco de Napoleão?", rq1, "azul", 1, 500, 1);
+		rq1.add("branco");
+		rq1.add("azul");
+		rq1.add("amarelo");
+		rq1.add("preto");
+		
+		//setando uma única pergunta
+		
 		new JanelaPerguntas();
+		pergunta.setText(q1.pergunta);
+		res[0].setText(rq1.get(0)); 
+		res[1].setText(rq1.get(1));
+		res[2].setText(rq1.get(2));
+		res[3].setText(rq1.get(3));
+		//
+		
+		List<String> rq2 = new ArrayList<String>();
+		Questao q2 = new Questao("pergunta2", rq2, "c", 1, 500, 2);
+		rq2.add("e");
+		rq2.add("e");
+		rq2.add("c");
+		rq2.add("e");
+		
+		List<String> rq3 = new ArrayList<String>();
+		Questao q3 = new Questao("pergunta3", rq3, "c", 1, 500, 3);
+		rq3.add("c");
+		rq3.add("e");
+		rq3.add("e");
+		rq3.add("e");
+		
+		List<String> rq4 = new ArrayList<String>();
+		Questao q4 = new Questao("pergunta4", rq4, "c", 1, 500, 4);
+		rq4.add("e");
+		rq4.add("c");
+		rq4.add("e");
+		rq4.add("e");
+		
+		List<String> rq5 = new ArrayList<String>();
+		Questao q5 = new Questao("pergunta5", rq5, "c", 1, 500, 5);
+		rq5.add("e");
+		rq5.add("e");
+		rq5.add("c");
+		rq5.add("e");
+		
+		List<String> rq6 = new ArrayList<String>();
+		Questao q6 = new Questao("pergunta6", rq6, "c", 1, 500, 6);
+		rq6.add("e");
+		rq6.add("e");
+		rq6.add("e");
+		rq6.add("c");
+		
+		List<String> rq7 = new ArrayList<String>();
+		Questao q7 = new Questao("pergunta7", rq7, "c", 1, 500, 7);
+		rq7.add("e");
+		rq7.add("e");
+		rq7.add("e");
+		rq7.add("c");
+		
+		List<String> rq8 = new ArrayList<String>();
+		Questao q8 = new Questao("pergunta8", rq8, "c", 1, 500, 8);
+		rq8.add("e");
+		rq8.add("e");
+		rq8.add("e");
+		rq8.add("c");
+		
+		List<String> rq9 = new ArrayList<String>();
+		Questao q9 = new Questao("pergunta9", rq9, "c", 1, 500, 9);
+		rq9.add("e");
+		rq9.add("e");
+		rq9.add("e");
+		rq9.add("c");
+		
+		
+		
 	}
 	
 	
