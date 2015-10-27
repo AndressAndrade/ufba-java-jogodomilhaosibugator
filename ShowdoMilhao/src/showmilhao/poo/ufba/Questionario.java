b2218e5d7f43bb5fdf69ee17776c4d3144709821
Andressa <andressa.brasil-tokyo@hotmail.com>
2015-10-27 16:12:40 -0300

    Mudancas de lista

--- ShowdoMilhao/src/showmilhao/poo/ufba/Questionario.java
+++ ShowdoMilhao/src/showmilhao/poo/ufba/Questionario.java
@@ -2,57 +2,81 @@ package showmilhao.poo.ufba;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import com.csvreader.CsvReader;
 
 public class Questionario {
+	
+	static List<Questao> listaQuestoes = new ArrayList();
+	static List<Questao> listaQuestoes2 = new ArrayList();
+	static List<Questao> listaQuestoes3 = new ArrayList();
+	static List<Questao> listaQuestoes4 = new ArrayList();
 
 	public static void main(String[] args) {
+		
 		try {
 			
-			CsvReader perguntas = new CsvReader("perguntas.csv");
+			CsvReader questoes = new CsvReader("perguntas.csv");
 		
-			perguntas.readHeaders();
+			questoes.readHeaders();
 			
 			String um = "1";
 			String dois = "2";
 			String tres = "3";
 			String quatro = "4";
 			
-			while (perguntas.readRecord())
+			while (questoes.readRecord())
 			{
-				String nivel = perguntas.get("nivel");
-				String pergunta = perguntas.get("pergunta");
-				String resposta1 = perguntas.get("resposta1");
-				String resposta2 = perguntas.get("resposta2");
-				String resposta3 = perguntas.get("resposta3");
-				String resposta4 = perguntas.get("resposta4");
-				String respostaCerta = perguntas.get("respostaCerta");
+				
+				String nivel = questoes.get("Nivel");
+				String pergunta = questoes.get("Pergunta");
+				String resposta1 = questoes.get("Resposta1");
+				String resposta2 = questoes.get("Resposta2");
+				String resposta3 = questoes.get("Resposta3");
+				String resposta4 = questoes.get("Resposta4");
+				String respostaCerta = questoes.get("RespostaCerta");
+				String valor = questoes.get("Valor");
+				String numeracao = questoes.get("Numeracao");
 				
 				if(um.equals(nivel)){
-					System.out.println(nivel + " : " + pergunta +" : " + resposta1 + " : " +resposta2 + " : " +resposta3 + " : " +resposta4+" : " +respostaCerta);
+					
+					//Questao questao = new Questao(nivel,pergunta,resposta1,resposta2,resposta3,resposta4,respostaCerta,valor,numeracao);
+					//listaQuestoes.add(questao);
+					
 				}
 				if(dois.equals(nivel)){
-					System.out.println(nivel);
+			
+					//Questao questao = new Questao(nivel,pergunta,resposta1,resposta2,resposta3,resposta4,respostaCerta,valor,numeracao);
+					//listaQuestoes2.add(questao);
 				}
 				if(tres.equals(nivel)){
-					System.out.println(nivel);
+					
+					//Questao questao = new Questao(nivel,pergunta,resposta1,resposta2,resposta3,resposta4,respostaCerta,valor,numeracao);
+					//listaQuestoes3.add(questao);
+					
 				}
 				if(quatro.equals(nivel)){
-					System.out.println(nivel);
+					
+					//Questao questao = new Questao(nivel,pergunta,resposta1,resposta2,resposta3,resposta4,respostaCerta,valor,numeracao);
+					//listaQuestoes4.add(questao);
 				}
-				
-				
 			}
 	
-			perguntas.close();
+			//embaralhando a lista de Questões
+			Collections.shuffle(listaQuestoes);
+			Collections.shuffle(listaQuestoes2);
+			Collections.shuffle(listaQuestoes3);
+			Collections.shuffle(listaQuestoes4);
+			
+			questoes.close();
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
-
 }
